4f49c934fab9ddb239c0353a281200ee4d9a0bd3
murilinhops <murilo.pescatori@nubank.com.br>
2023-08-29 15:24:28 -0300

    Finish Hash studies

--- src/hash_maps.cr
+++ src/hash_maps.cr
@@ -5,8 +5,6 @@ tools = {
   "Android" => "Kotlin/Java",
   "IOS"     => "Swift/Objective-C",
 }
-puts tools.keys
-puts tools.values
 
 planets = {
   "Marte"   => 1,
@@ -14,6 +12,18 @@ planets = {
   "Saturno" => 3,
   "Terra"   => 4,
 }
+
+space = {
+  "Marte"   => 1,
+  "Júpiter" => 2,
+  "Saturno" => 3,
+  "Terra"   => 4,
+  "Lua"     => 5,
+}
+
+puts tools.keys
+puts tools.values
+
 puts planets,
   planets.size, # a quantidade de itens, 4
   planets.keys  # chaves de cada item
@@ -30,6 +40,7 @@ palavras = ["gato", "cachorro", "cobra", "gato", "joaninha", "formiga", "gato", 
 count = {} of String => Int32                                                               # Hash vazio
 def palavras_count_hash(arr : Array(String)) : Hash(String, Int32)
   hash = {} of String => Int32
+
   arr.each { |palavra|
     if !hash.has_key?(palavra) # o hash tem a palavra? se não tiver, adiciona o valor 0 nela
       hash[palavra] = 0
@@ -38,10 +49,56 @@ def palavras_count_hash(arr : Array(String)) : Hash(String, Int32)
   }
   return hash
 end
+
 count = palavras_count_hash(palavras)
 puts count
 count.each { |palavra, qnt|
   puts "#{palavra}: #{qnt}"
 }
 
+puts planets,
+  space
 
+# merge
+more_planets = planets.merge({"Vênus" => 5, "Marte" => 10}) # returns a new hash with these two new values, planets stays the same
+puts more_planets
+puts planets # .merge não modifica o hash original
+
+puts space.merge!({"Plutão" => 5, "Marte" => 20}) # metodo com ! modifica ele mesmo, side effect
+puts space
+
+# Manipulate Hash
+puts planets.delete("Marte")
+puts planets # {"Júpiter" => 2, "Saturno" => 3, "Terra" => 4, "Plutão" => 7}
+planets["Marte"] = 1
+puts " "
+
+puts planets.reject("Marte")                                           # retorna todos do mapa que nãoo sejam Marte, tem sua versão com side effect !
+puts planets.select { |name, value| value > 2 && name.includes?("r") } # {"Saturno" => 3, "Terra" => 4}, não moidifica o hash e tem sua versao com !
+
+planets.clear # limpa o hash inteiro => {}
+puts planets,
+  ""
+
+planetas = {
+  "Marte" => {
+    "cor"    => "Vermelho e marrom...",
+    "number" => 1,
+  },
+  "Júpiter" => {
+    "cor"    => "Marrom e laranja...",
+    "number" => 2,
+  },
+  "Saturno" => {
+    "cor"    => "Dourado, marrom e com tons de verde...",
+    "number" => 3,
+  },
+  "Terra" => {
+    "cor"    => "Azul, marrom, verde e com tons de branco...",
+    "number" => 4,
+  },
+}
+puts planetas.each { |key, value|
+  color = value["cor"] # funciona como pegar o valor de um json
+  puts "#{key}: sua cor é #{color}"
+}
