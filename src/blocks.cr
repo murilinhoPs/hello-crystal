2d1dd2ba713bb28d68ecc328b7dd88b72f49d30c
murilinhops <murilinhorps@gmail.com>
2023-09-28 14:04:48 -0400

    Finish block and procs studies

--- src/blocks.cr
+++ src/blocks.cr
@@ -1,5 +1,5 @@
 # ?Cada yield chama o bloco de código, chama twice pq exeuta duas vezes não por causa de kpop
-def twice(&) # *& define um bloco de codigo que vai receber
+def twice(&) # *& define um bloco(block) de codigo que vai receber
   yield      # *executa o bloco de código
   yield      # *executa o bloco de código
 end
@@ -75,13 +75,166 @@ def pairs(&)
   yield 3, 6
 end
 
-pairs do |_, second| # *Ommit the first parameter
+pairs do |_, second| # *Ommit the first parameter or can ommit second parameter
   print second
 end
 
 # *Short syntax => block_method &.some_method, fazer algo direto com o primeiro argumento, ele é passado como argumento para o some_method
+# method do |param|
+#    something
+# end
+# method &.something (& is the block parameter)
+# methos(&.something)
 
 puts ["a", "b"].join(",") { |s| s.upcase }
 puts ["a", "b"].join(",", &.upcase)
 # method &.+(2), somar mais 2 no valor
-# method(&.[index]), passar como index
+
+# ? usually blocks methods are used to transform values, like below
+# ? enumerableMap e enumerableSelect são exemplos de blocks
+arry = [1, 2, 3]
+puts arry.map { |x| x + 1 }         # => [2, 3, 4]
+puts arry.select { |x| x % 2 == 1 } # => [1, 3]
+
+# Returns o value para poder modificá-lo
+
+def transform(value, &)
+  yield value # value is the argumento
+end
+
+puts transform(1) { |x| x + 1 } # => 2
+
+# *Accepts only ints, and expects bloc receive int as parameter and return an int too
+def transform_ints(start : Int32, &block : Int32 -> Int32)
+  result = yield start # the argument
+  result * 2
+end
+
+puts transform_ints(2) { |x| x + 1 } # => 6 => (2 +1) = 3 => 3 *2 => 6
+
+# *break and next
+
+def thrice(&)
+  puts "Before 1"
+  yield 1
+  puts "Before 2"
+  yield 2
+  puts "Before 3"
+  yield 3
+end
+
+thrice do |i| # will only puts Before 1 and Before 2 bc of break == 2
+  if i == 2
+    break
+  end
+end
+
+def twice(&)
+  yield 1
+  yield 2
+end
+
+puts twice { |i| i + 1 }         # => 3
+puts twice { |i| break "hello" } # => will stops/breaks the method and return "hello"
+puts twice { break 3, 4 }        # => {3, 4} : Tuple
+p! twice { break }               # => nil
+
+# ? will not return Got 1, bc when == 1 we're skpping that with next and do another thing
+# ? continue the method and exit from that block
+puts twice { |i|
+  if i == 1
+    puts "Skipping 1"
+    next
+  end
+  puts "Got #{i}"
+} # => Skipping 1 Got 2
+# Wonderland (rua)
+
+# *with ... yield
+
+class WithYield
+  def one
+    1
+  end
+
+  # *returns the method inside it, modify what to yields
+  def yield_with_self(&)
+    with self yield
+  end
+
+  # *returns normally the method passed to the block
+  def yield_normally(&)
+    yield
+  end
+end
+
+def one
+  "one"
+end
+
+puts WithYield.new.yield_with_self { one } # => 1, the method inside the block
+puts WithYield.new.yield_normally { one }  # => "one"
+
+new_array = [{1, "one"}, {2, "two"}]
+new_array.each do |(number, word)| # * sub-parameters of the array. |number, word|, se for tuple pode fazer assim
+  puts "#{number}: #{word}"
+end
+# * não preciso usar sub-paramters, pode ser arg e extrair pelo index dele
+new_array.each do |args|
+  number = args[0]
+  word = args[1]
+  puts "#{number}: #{word}"
+end
+
+other_arr = [[1, "one"], [2, "two"]]
+other_arr.each do |(number, word)|
+  puts "#{number}: #{word}"
+end
+
+def int_to_int(&block : Int32 -> Int32)
+  block
+end
+
+# * Captured the block as a proc and called it, only works with a type on the &block
+proc = int_to_int { |x| x + 1 } # ? type = Proc(Int32, Int32), recebe um int e retorna outro int
+puts proc.class
+p! proc.call 1 # => 2
+
+class Model
+  def on_save(&block : -> String) # ? returns only a string
+    @on_save_callback = block
+  end
+
+  def save
+    # * se o callback não existir, o on_save_callback is nil
+    if callback = @on_save_callback
+      callback.call
+    end
+  end
+end
+
+model = Model.new
+model.on_save do
+  saved = "Saved"
+  puts saved
+  saved
+end
+p! model.save.class
+model.save
+
+def wrong_proc(&block : Int32 ->) #! não tem retorno, se quiser um retorno precisa especificar um tipo ou _ (qualquer retorno)
+  block
+end
+
+proc = wrong_proc { |x| x + 1 }
+p! proc.call 1 # => returns nil, pq o block não tem retorno (nil) só tem argumento
+
+def correct_proc(&block : Int32 -> _) # ! tem um retorno de qualquer tipo
+  block
+end
+
+proc = correct_proc { |x| x + 1 } # => Proc(Int32, Int32)
+p! proc.call(1)                   # 2
+
+proc = correct_proc { |x| x.to_s } # => Proc(Int32, String)
+p! proc.call(1)                    # "1"
--- src/procs.cr
+++ src/procs.cr
@@ -37,7 +37,6 @@ num_proc(x: 7, y: 3, proc: int_divide_proc)
 
 num_proc(x: 7, y: 3, proc: ->(n1 : Int32, n2 : Int32) { n1 // n2 }) # // arredonda pra baixo
 
-
 puts "-------------------"
 
 # OU
@@ -45,7 +44,7 @@ def sum(x : Int32, y : Int32)
   x + y
 end
 
-def_sum_proc = ->sum(Int32, Int32) # reference the function sum
+def_sum_proc = ->sum(Int32, Int32)       # reference the function sum
 num_proc(x: 2, y: 4, proc: def_sum_proc) # => 6
 
 def minus(x : Int32, y : Int32)
@@ -72,10 +71,95 @@ num_proc(x: 3, y: 6, proc: def_divide_proc) # => 2
 puts "---------String----------"
 str = "hello"
 str_proc = ->str.count(Char) # proc, posso especificar qual funcao chamar mesmo uma ja existente da string por exemplo
-puts str_proc.call('e') # => 1 só tem um E
-puts str_proc.call('l') # => 2, tem dois LL's
+puts str_proc.call('e')      # => 1 só tem um E
+puts str_proc.call('l')      # => 2, tem dois LL's
 
 puts "------Block-Proc-------"
 
+# ! is the same as declaring a proc and passing as a block parameter
+
 def num_proc_block(&block : Int32 -> Int32)
+  block
 end
+
+captured_block = num_proc_block { |x| x += 1 }
+p! captured_block.call 2
+
+# https://crystal-lang.org/reference/1.9/syntax_and_semantics/closures.html
+x = 0                                   # ! closure the X, o valor do X continua existindo e mudando dentro da proc enquanto a proc existir
+num_proc = ->(int : Int32) { x += int } # *passo o int como param e somo ao X
+block_proc = num_proc_block(&num_proc)
+
+p! num_proc.call 2    # => 2
+p! block_proc.call 10 # => 12
+puts x                # => 12
+
+# ? Is the same as above, just extracting the method
+
+def add(x, y)
+  x + y
+end
+
+adder = ->add(Int32, Int32)
+p! adder.call(1, 2) # => 3
+
+puts "------Forwarding Captured Blocks-------"
+
+def capture(&block)
+  block
+end
+
+def invoke(&block)
+  block.call
+end
+
+captured = capture { puts "Captured" }
+invoke(&captured)
+
+def twice(&)
+  yield
+  puts "or"
+  yield
+end
+
+twice_proc = capture { puts "Yes!" }
+twice &twice_proc
+# or
+twice do
+  twice_proc.call
+end
+# or
+twice &->{ puts "Twice!" }
+
+# or
+def puts_twice
+  puts "Puts Twice!"
+end
+
+twice &->puts_twice
+
+puts "------Forwarding NON-Captured Blocks-------"
+
+def only_twice(&)
+  yield "Twice! 🎵"
+end
+
+def wrap_only_twice(&)
+  puts "Before twice"
+  only_twice do |param| # ? proprio parameter dele
+    yield param
+  end
+  puts "After twice"
+end
+
+wrap_only_twice do |i|
+  puts i # *vai printar o retorno do método only_twice, que é o yield "Twice"
+end
+
+puts "------With alias-------"
+just_proc = Proc(Int32, String).new { |value| value.to_s }
+just_proc.call 3
+
+alias ProcAlias = Int32 -> String
+proc_alias = ProcAlias.new { |value| value.to_s }
+puts proc_alias.call 2
